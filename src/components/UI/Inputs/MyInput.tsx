5abee65583ac046feb1f9e1e68e57a1be25bf992
Abdyukov Zufar <swat55551@gmail.com>
2024-01-22 15:06:29 +0300

    Set style by ESLINT and Prettier

--- src/components/UI/Inputs/MyInput.tsx
+++ src/components/UI/Inputs/MyInput.tsx
@@ -1,57 +1,55 @@
-
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction } from 'react';
 import { Form } from 'react-bootstrap';
 
-function trimSpaces(input: string): string 
-{
-    return input.trim();
+function trimSpaces(input: string): string {
+  return input.trim();
 }
-const debounce = (fn: Function, ms = 300,setLoading?:Dispatch<SetStateAction<boolean>> | undefined) => 
-{
-    let timeoutId: ReturnType<typeof setTimeout>;
-    return function (this: unknown, ...args: unknown[]) 
-    {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => 
-        {
-            fn.apply(this, args)
-            if(setLoading)
-                setLoading(false);
-        }, ms);
-        if(setLoading)
-            setLoading(true);
-    };
+const debounce = (
+  fn: Function,
+  ms = 300,
+  setLoading?: Dispatch<SetStateAction<boolean>> | undefined
+) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: unknown, ...args: unknown[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn.apply(this, args);
+      if (setLoading) setLoading(false);
+    }, ms);
+    if (setLoading) setLoading(true);
+  };
 };
 
-
-export const MyInput = (
-    {titleLabel,placeholder,_defaultValue,setState,type='text',setStateLoading=undefined}:
-    {
-        titleLabel:string,
-        placeholder:string,
-        _defaultValue:string,
-        setState:Dispatch<SetStateAction<string>>
-        type?:string,
-        setStateLoading?:Dispatch<SetStateAction<boolean>>
-
-    }) => 
-{
-    const debounced = debounce(setState,3000,setStateLoading);
-    return (
-        <> 
-        <Form.Group>
-            <Form.Label>{titleLabel}</Form.Label>
-                <Form.Control 
-                type={type} 
-                placeholder={placeholder}
-                defaultValue={_defaultValue}
-                onChange={e=>
-                {
-                    trimSpaces(e.target.value);
-                    debounced(e.target.value);
-                }} />
-        </Form.Group>
-        </>
-
-    )
-}
+export const MyInput = ({
+  titleLabel,
+  placeholder,
+  _defaultValue,
+  setState,
+  type = 'text',
+  setStateLoading = undefined
+}: {
+  titleLabel: string;
+  placeholder: string;
+  _defaultValue: string;
+  setState: Dispatch<SetStateAction<string>>;
+  type?: string;
+  setStateLoading?: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const debounced = debounce(setState, 3000, setStateLoading);
+  return (
+    <>
+      <Form.Group>
+        <Form.Label>{titleLabel}</Form.Label>
+        <Form.Control
+          type={type}
+          placeholder={placeholder}
+          defaultValue={_defaultValue}
+          onChange={(e) => {
+            trimSpaces(e.target.value);
+            debounced(e.target.value);
+          }}
+        />
+      </Form.Group>
+    </>
+  );
+};
--- src/components/UI/Popup/utils.ts
+++ src/components/UI/Popup/utils.ts
@@ -1,41 +1,39 @@
-import { setLocalStorageItem } from "../../../utils/helper";
+import { setLocalStorageItem } from '../../../utils/helper';
 import { IInputs, ISetInputs } from '../../../store/interfaces/IInputs';
 import { IDataUser } from '../../../store/interfaces/IDataUser';
 
-const dataFromStringToArray = (data:string,separator:string=',') =>
-{
-    return data.split(separator);
-}
+const dataFromStringToArray = (data: string, separator: string = ',') => {
+  return data.split(separator);
+};
 
-export const saveChanges = (user:IDataUser,data:IInputs,handleClose:(() => void) | undefined):void =>
-{
-    user.setLogin(data.loginInput);
-    user.setRepo(data.repoInput);
-    user.setBlacklist(dataFromStringToArray(data.blacklistInput));
-    setLocalStorageItem('userdata',data.loginInput + ';' + data.repoInput + ';' + data.blacklistInput);
-    if(handleClose)
-        handleClose();
-}
+export const saveChanges = (
+  user: IDataUser,
+  data: IInputs,
+  handleClose: (() => void) | undefined
+): void => {
+  user.setLogin(data.loginInput);
+  user.setRepo(data.repoInput);
+  user.setBlacklist(dataFromStringToArray(data.blacklistInput));
+  setLocalStorageItem(
+    'userdata',
+    data.loginInput + ';' + data.repoInput + ';' + data.blacklistInput
+  );
+  if (handleClose) handleClose();
+};
 
-export const fillInputs = (user:IDataUser,data:ISetInputs) =>
-{
-    if(user.blacklist.length!==0)
-    {
-        let blacklistText = '';
-        for(let i =0;i<user.blacklist.length; i++)
-            if(i+1 !== user.blacklist.length)
-                blacklistText += user.blacklist[i] + ',';
-            else
-                blacklistText += user.blacklist[i]
-        
-        data.setBlacklistInput(blacklistText);
-    }
-    if(user.login !== '')
-    {
-        data.setLoginInput(user.login);
-    }
-    if(user.repo !== '')
-    {
-        data.setRepoInput(user.repo);
-    }
-}
+export const fillInputs = (user: IDataUser, data: ISetInputs) => {
+  if (user.blacklist.length !== 0) {
+    let blacklistText = '';
+    for (let i = 0; i < user.blacklist.length; i++)
+      if (i + 1 !== user.blacklist.length) blacklistText += user.blacklist[i] + ',';
+      else blacklistText += user.blacklist[i];
+
+    data.setBlacklistInput(blacklistText);
+  }
+  if (user.login !== '') {
+    data.setLoginInput(user.login);
+  }
+  if (user.repo !== '') {
+    data.setRepoInput(user.repo);
+  }
+};
